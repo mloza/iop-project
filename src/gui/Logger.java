471fd7df8fa65b38debe4c97a3ce8f234c3bf38e
Paweł Góralik <pawel.goralik@gmail.com>
2012-09-27 20:52:49 +0200

    Aplication.CURRENT_DIRECTORY określa ścieżkę koło pliku JAR lub jeśli uruchamiamy z Eclipse to folder iop-project. Jeśli korzystacie z jakiś zew. danych, to należy względem CURRENT_DIRECTORY to robić. Trochę uporządkowałem kod klasy Main -> Application.

---
+++ src/gui/Logger.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Logger {
+	private static PrintWriter out;
+	
+	public static void create(String destinationPath) throws IOException {
+		out = new PrintWriter(new BufferedWriter(new FileWriter(destinationPath)));
+	}
+	
+	public static void log(String msg) {
+		if(out != null) {
+			out.println(msg);
+			out.flush();
+		}
+		
+	}
+	
+	public static void logException(Exception e) {
+		if(out != null) {
+			out.println("GOT EXCEPTION: ");
+			e.printStackTrace(out);
+			out.println("");
+			out.flush();
+		}
+		
+	}
+}
